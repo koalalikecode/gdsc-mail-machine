f1d96c30c74c437bafbf77e768bcdf081d949c7d
Duy Nguyễn <duykhanhchi1993@gmail.com>
2023-10-31 17:19:20 +0700

    setup mail tool

---
+++ README.md
@@ -0,0 +1,77 @@
+# Mailer Machine
+
+## How to run?
+
+**Make sure you already installed Node**
+
+## Steps
+
+### 1. Install Required Packages
+
+Use either of the following commands to install the required packages:
+
+`npm add .`
+
+### 2. Set Up Environment Variables
+
+Create a file named .env and include your email credentials as follows:
+
+```js
+EMAIL=example@example.com
+PASSWORD=example-password
+CUSTOM_MAIL_NAME=example (optional)
+```
+
+Note: If you are using Gmail as your mail provider, you should use an application password to avoid errors.
+
+### 3. Set Up Mail Content and Recipient List
+
+#### Mail options
+
+> Customize your mail options in server.js file or leave it default and add mail subject.
+
+```js
+const mailOptions = {
+  from: `${process.env.CUSTOM_MAIL_NAME} <${process.env.EMAIL}>`,
+  to: recipientMail,
+  subject: ``,
+  text: content,
+  html: content,
+};
+```
+
+#### Mail content
+
+> Insert your mail content in the content.js file. You can use either HTML or plain text email. Moreover, you can use altContent in server.js ` const altContent = ``; `, remember to convert [HTML to escaped text](https://onlinetexttools.com/escape-text)
+
+#### Recipient list
+
+> Create a .csv file with a list of recipients in the following format:
+
+```
+email,name
+example@gmail.com,example name
+```
+
+### 4. Run the Tool
+
+Use one of the following commands to run the tool:
+
+`node server.js --csvFile={nameOfRecipientListFile}.csv --isHtmlMail={boolean}`
+
+or
+
+`npm run text`
+
+or
+
+`npm run html`
+
+### Note
+
+> Limitation for email quantity sending per day is depended on mail provider you using.
+
+## Author
+
+[ndbac (Nguyễn Đình Bắc)](https://github.com/ndbac)
+[koalalikecode (Nguyễn Duy)][https://github.com/koalalikecode]
---
+++ server.js
@@ -0,0 +1,84 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const csvToJson = require("csvToJson");
+const minimist = require("minimist");
+const path = require("path");
+const _ = require("lodash");
+const { content } = require("./content");
+
+const sendMail = async ({
+  recipientMail,
+  recipientName,
+  content,
+  isHtmlMail,
+  transporter,
+}) => {
+  const mailOptions = {
+    from: `${process.env.CUSTOM_MAIL_NAME} <${process.env.EMAIL}>`,
+    to: recipientMail,
+    subject: ``,
+    text: content,
+    html: content,
+  };
+  try {
+    await transporter.sendMail({
+      ...mailOptions,
+      text: isHtmlMail == "true" ? undefined : content,
+      html: isHtmlMail == "true" ? content : undefined,
+    });
+    console.log(`Email sent to ${recipientMail} (${recipientName})!`);
+  } catch (error) {
+    console.error(
+      `Failed to send mail to ${recipientMail} (${recipientName})!`,
+      error
+    );
+  }
+};
+
+const parseCommandArgs = (opts, selectKeys) => {
+  const res = minimist(process.argv.slice(2), opts);
+  if (!selectKeys) {
+    return res;
+  }
+  return _.pick(res, selectKeys);
+};
+
+// node server.js --csvFile=input.csv --isHtmlMail=true
+// csv file must have 2 columns name 'email' and 'name' indicate for recipient email and name
+const run = async () => {
+  console.log("Starting sending email...");
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD, // should use app password if you use Gmail as mail provider
+    },
+  });
+
+  const args = parseCommandArgs();
+  const data = await csvToJson().fromFile(
+    path.resolve(process.cwd(), args.csvFile || "input.csv")
+  );
+
+  await Promise.all(
+    data.map((rec) => {
+      const altContent = ``;
+      return sendMail({
+        recipientMail: rec.email,
+        recipientName: rec.name,
+        content: altContent || content,
+        isHtmlMail: args.isHtmlMail,
+        transporter,
+      });
+    })
+  );
+  console.log(process.env.CUSTOM_MAIL_NAME);
+  console.log("Finishing sending email...");
+};
+
+run()
+  .then(() => process.exit(0))
+  .catch((e) => {
+    console.error(JSON.stringify(e));
+    process.exit(-1);
+  });
